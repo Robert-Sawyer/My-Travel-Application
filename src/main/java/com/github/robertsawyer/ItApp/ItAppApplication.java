24f13305a2e166235790552b98467947e71b9da0
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-04-15 15:06:46 +0200

    Commit

---
+++ src/main/java/com/github/robertsawyer/ItApp/ItAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.robertsawyer.ItApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItAppApplication.class, args);
+	}
+
+}
