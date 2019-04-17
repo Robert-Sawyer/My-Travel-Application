4c948f96bf724c13f3328397ec554333188aa8e4
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-04-17 12:19:57 +0200

    Utworzenie Place DTO.

--- src/main/java/com/github/robertsawyer/ItApp/dtos/PlaceDTO.java
+++ src/main/java/com/github/robertsawyer/ItApp/dtos/PlaceDTO.java
@@ -1,4 +1,17 @@
 package com.github.robertsawyer.ItApp.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlaceDTO {
+
+    private String name;
+    private String country;
+    private String city;
+    private String description;
+
 }
