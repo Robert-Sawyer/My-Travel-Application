b2cd65f59538aae149f5daa52b654a3d818a2dd1
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-04-17 14:21:35 +0200

    Utworzenie kontrolera planu podróży.

--- src/main/java/com/github/robertsawyer/ItApp/web/controllers/TravelPlanController.java
+++ src/main/java/com/github/robertsawyer/ItApp/web/controllers/TravelPlanController.java
@@ -1,6 +1,8 @@
 package com.github.robertsawyer.ItApp.web.controllers;
 
-import com.github.robertsawyer.ItApp.dtos.TravelPlanDTO;
+import com.github.robertsawyer.ItApp.dtos.AddTravelPlanDTO;
+import com.github.robertsawyer.ItApp.services.PlaceService;
+import com.github.robertsawyer.ItApp.services.PlanService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,15 +18,29 @@ import java.security.Principal;
 @RequestMapping("travel-plan")
 public class TravelPlanController {
 
+    private PlaceService placeService;
+
+    private PlanService planService;
+
+    public TravelPlanController(PlaceService placeService, PlanService planService) {
+        this.placeService = placeService;
+        this.planService = planService;
+    }
+
     @GetMapping
     public String prepareAddNewPlanForm(Model model){
-        model.addAttribute("travelPlan", new TravelPlanDTO());
+        model.addAttribute("travelPlan", new AddTravelPlanDTO());
         return "/travels/AddPlanForm";
     }
 
     @PostMapping
-    public String addNewPlan(@Valid @ModelAttribute("travelPlan") TravelPlanDTO travel, BindingResult result, Principal principal){
+    public String addNewPlan(@Valid @ModelAttribute("travelPlan") AddTravelPlanDTO addTravelPlanDTO, BindingResult result, Principal principal){
         String name = principal.getName();
+        if (result.hasErrors()){
+            return "travels/AddPlanForm";
+        }
+        planService.createTravelPlan(addTravelPlanDTO);
         return "redirect:dashboard";
     }
+
 }
