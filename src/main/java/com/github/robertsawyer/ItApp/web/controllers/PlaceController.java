4a8f81686940d853b6102d1288762aefec2e0318
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-06-22 13:07:24 +0200

    Added adnotation Autowired, paths in Get/PostMapping, methods findPlace and addPlaceToPlan.

--- src/main/java/com/github/robertsawyer/ItApp/web/controllers/PlaceController.java
+++ src/main/java/com/github/robertsawyer/ItApp/web/controllers/PlaceController.java
@@ -1,8 +1,16 @@
 package com.github.robertsawyer.ItApp.web.controllers;
 
+import com.github.robertsawyer.ItApp.domain.model.Place;
+import com.github.robertsawyer.ItApp.domain.model.TravelPlan;
 import com.github.robertsawyer.ItApp.dtos.AddPlaceDTO;
+import com.github.robertsawyer.ItApp.dtos.AddPlaceToPlanDTO;
+import com.github.robertsawyer.ItApp.dtos.FindPlaceDTO;
+import com.github.robertsawyer.ItApp.dtos.TravelPlanDTO;
 import com.github.robertsawyer.ItApp.services.PlaceService;
 import com.github.robertsawyer.ItApp.services.PlanService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,8 +26,12 @@ import java.security.Principal;
 @RequestMapping("/places")
 public class PlaceController {
 
+    public static final Logger logger = LoggerFactory.getLogger(PlaceController.class);
+
+    @Autowired
     private PlaceService placeService;
 
+    @Autowired
     private PlanService planService;
 
     public PlaceController(PlaceService placeService, PlanService planService) {
@@ -27,13 +39,13 @@ public class PlaceController {
         this.planService = planService;
     }
 
-    @GetMapping
+    @GetMapping("/add")
     public String prepareAddNewPlaceForm(Model model) {
         model.addAttribute("place", new AddPlaceDTO());
         return "places/AddPlaceForm";
     }
 
-    @PostMapping
+    @PostMapping("/add")
     public String addNewPlace(@Valid @ModelAttribute("place") AddPlaceDTO addPlaceDTO, BindingResult result, Principal principal){
         String name = principal.getName();
         if (result.hasErrors()) {
@@ -43,18 +55,39 @@ public class PlaceController {
             result.rejectValue("country", null, "Taki kraj nie istnieje");
             return "places/AddPlaceForm";
         }
+        logger.info("Zapisuję miejsce: " + addPlaceDTO);
         placeService.createPlace(addPlaceDTO);
         return "redirect:/dashboard";                                         //jeśli kraj istnieje to zapisze miejsce i skieruje do kokpitu
     }
 
-//    @GetMapping
-//    public String showPlacesList(Model model, Principal principal){
-//        String name = principal.getName();
-//        model.addAttribute("places", placeService.getAllPlaces());
-//        //dodać logikę wyświetlającą listę miejsc nie związanych z żadnym z planów zalogowanego użytjownika
-//        return "places/listAllPlaces";
-//    }
+    @GetMapping("/listAllPlaces")
+    public String showPlacesList(Model model){
+        model.addAttribute("placeName", new FindPlaceDTO());
+        model.addAttribute("places", placeService.getAllPlaces());
+        model.addAttribute("plans", planService.getAllTravelPlans());
+        model.addAttribute("addPlaceToPlan", new AddPlaceToPlanDTO());
+        //dodać logikę wyświetlającą listę miejsc nie związanych z żadnym z planów zalogowanego użytjownika
+        return "places/listAllPlaces";
+    }
 
+    @PostMapping(value = "/listAllPlaces", params = "search")
+    public Place findPlace(@ModelAttribute("placeName") FindPlaceDTO placeName){
+        return placeService.findByName(placeName);
+    }
+
+    @PostMapping(value = "/listAllPlaces", params = "addToPlan")
+    public String addPlaceToPlan(@ModelAttribute("addPlaceToPlan") AddPlaceToPlanDTO placeToPlan){
+        logger.info(placeToPlan.toString());
+        planService.addPlaceToPlan(placeToPlan);
+        return "redirect:/places/listAllPlaces";
+    }
+
+
+
+//    @PostMapping("/listAllPlaces")
+//    public TravelPlanDTO showPlanName(@ModelAttribute("plans") TravelPlanDTO planName){
+//        return planService.findByName(planName);
+//    }
 
 
     private boolean checkCountryExists(AddPlaceDTO addPlaceDTO) {
