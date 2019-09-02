f6c11f8dbac0ce8d378878c5859f60d226484585
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-09-02 21:29:40 +0200

    Added getters and setters in RegistrationFormDTO. Modified pom.xml

--- src/main/java/com/github/robertsawyer/ItApp/dtos/RegistrationFormDTO.java
+++ src/main/java/com/github/robertsawyer/ItApp/dtos/RegistrationFormDTO.java
@@ -30,4 +30,37 @@ public class RegistrationFormDTO {  //klasa zawierajaca pola niezbędne do obsł
 //    private String firstName;
 //    @NotBlank
 //    private String lastName;
+
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
