cb6d6840c3f837f4bac966f2ca1991577078de9f
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-09-04 19:46:35 +0200

    Modified redirect in method.

--- src/main/java/com/github/robertsawyer/ItApp/web/controllers/RegistrationController.java
+++ src/main/java/com/github/robertsawyer/ItApp/web/controllers/RegistrationController.java
@@ -44,7 +44,7 @@ public class RegistrationController {
         }
         //Konwersja rfDTO na user encja i wywołąnie userrepositiry.save(...);
         userService.registerUser(form);
-        return "redirect:/index.html";
+        return "redirect:/home";
     }
 
     private boolean checkIsUsernameIsAvailable(RegistrationFormDTO form) {
